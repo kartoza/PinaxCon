11a6badc894146c358981e9e0cb4c6fbe7031859
Volker Mische <volker.mische@gmail.com>
2016-01-20 09:41:39 +0100

    Add word counter to abstract text field
    There's no a little textbox displayed below the form which shows the
    current count of words.

--- static/dist/js/site.js
+++ static/dist/js/site.js
@@ -55,6 +55,27 @@
 
 	__webpack_require__(117);
 
+	$(document).ready(function () {
+	    // Display a small box that counts the number of words
+	    if (Array.prototype.filter) {
+	        var counter = $('<input type="text" value="0" class="form-control"' + 'style="float:right;width:3.75em;text-align:right;margin-top:5px" ' + 'readonly="readonly"/>');
+	        var textfield = $('#id_abstract');
+	        textfield.after(counter);
+
+	        var charachter_re = /[a-zA-Z0-9]/;
+	        textfield.on('input propertychange', function (evt) {
+	            var words = $(this).val().split(/\s+/);
+	            words = words.filter(function (word) {
+	                return word.match(charachter_re);
+	            });
+	            counter.val(words.length);
+	        });
+
+	        // Populate the field with the current number of words
+	        textfield.trigger('propertychange');
+	    }
+	});
+
 /***/ },
 
 /***/ 88:
--- static/src/js/main.js
+++ static/src/js/main.js
@@ -5,3 +5,28 @@ require("bootstrap");
 require("../less/site.less");
 
 require("../images/foss4g-logo-150px-small.png");
+
+
+$(document).ready(function() {
+    // Display a small box that counts the number of words
+    if (Array.prototype.filter) {
+        var counter = $(
+            '<input type="text" value="0" class="form-control"' +
+                'style="float:right;width:3.75em;text-align:right;margin-top:5px" ' +
+                'readonly="readonly"/>');
+        var textfield = $('#id_abstract');
+        textfield.after(counter);
+
+        var charachter_re = /[a-zA-Z0-9]/;
+        textfield.on('input propertychange', function(evt) {
+            var words = $(this).val().split(/\s+/);
+            words = words.filter(function(word) {
+                return word.match(charachter_re);
+            });
+            counter.val(words.length);
+        });
+
+        // Populate the field with the current number of words
+        textfield.trigger('propertychange');
+    }
+});
